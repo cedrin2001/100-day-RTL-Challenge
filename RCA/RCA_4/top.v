bd807a4cd87532ade21450441e392385bd1fbfde
cedrin2001 <67495598+cedrin2001@users.noreply.github.com>
2025-03-25 22:00:14 +0530

    Update top.v

--- RCA/RCA_4/top.v
+++ RCA/RCA_4/top.v
@@ -1,6 +1,6 @@
 module top;
-wire [7:0]a,b,sum;
+wire [3:0]a,b,sum;
 wire carry,cin;
-RCA_8 dut(a,b,cin,sum,carry);
-RCA_8_tb TB(a,b,cin,sum,carry);
+RCA dut(a,b,cin,sum,carry);
+RCA_tb TB(a,b,cin,sum,carry);
 endmodule
